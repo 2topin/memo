aa1a404720ddcd5b63822993c111206e91418b98
Lim Suyoung <leepari20@gmail.com>
2023-06-05 12:41:28 +0900

    개쓰레기코드3

--- Input.java
+++ Input.java
@@ -1,61 +1,18 @@
 package memo;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class Input {
-    ArrayList<Input> memoList = new ArrayList<>();
-    String name;
-    String password;
-    String text;
-
-    Input( String name, String password, String text) {
-        this.name = name;
-        this.password = password;
-        this.text = text;
+    Map<Integer, Memo> memoList = new LinkedHashMap<>();
+    public void inputMemo(int textNum, String name, String password, String text) {
+        Memo memo = new Memo(name, password, text);
+        memoList.put(textNum, memo);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public ArrayList<Input> getMemoList() {
-        return memoList;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public void setMemoList(ArrayList<Input> memoList) {
-        this.memoList = memoList;
-    }
-
-    public void addList(int textNum) {
-        Input inputList = new Input(getName(), getPassword(), getText());
-        memoList.add(textNum, inputList);
-    }
-
-    public void printList() {
-        Collections.reverse(getMemoList());
-        for (Input i : getMemoList()) {
-            System.out.println(i.getName() + i.getPassword() + i.getText());
+    public void printMemo() {
+        Set<Integer> keySet = memoList.keySet();
+        for (Integer key : keySet) {
+            System.out.println(key + " : " + memoList.get(key));
         }
     }
 }
